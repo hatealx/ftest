49e8d1090aa1f8f4110235c26023359343017285
abel <you@example.com>
2024-07-29 11:15:47 +0000

    learn how to implement a BottomNavigationBar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'fly.dart';
 
+import 'gratitude.dart';
+import 'birthdays.dart';
+import 'reminders.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -8,37 +10,52 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  
+  int _currentIndex = 0;
+
+  List _listPages = [];
+  late Widget _currentPage;
+
+  void _changePage(int selectedIndex) {
+    setState(() {
+      _currentIndex = selectedIndex;
+      _currentPage = _listPages[selectedIndex];
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _listPages
+      ..add(Birthdays())
+      ..add(Gratitude())
+      ..add(Reminders());
+    _currentPage = Birthdays();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Hero Navigation Animation"),    
+        title: const Text("Hero Navigation Animation"),
       ),
       body: SafeArea(
-        child:Padding(
+        child: Padding(
           padding: EdgeInsets.all(16.0),
-          child: GestureDetector(
-            child: const Hero(
-              tag: "format_paint", 
-              child: Icon(
-                Icons.format_paint,
-                color: Colors.lightBlue,
-                size: 120.0,
-              )
-              ),
-              onTap: () {
-                Navigator.push(
-                  context, MaterialPageRoute(
-                    builder: (context) => Fly() 
-                    )
-                );
-              },
-          ),
+          child: _currentPage,
         ),
-        )
-
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.cake), label: "Birthdays"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.sentiment_satisfied), label: "Gratitude"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.access_alarm), label: "Remnders")
+        ],
+        onTap: (selectedIndex) => _changePage(selectedIndex),
+      ),
     );
   }
 }
