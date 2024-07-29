cfb61efde2ea78783b324456b35f34abfa319bb1
abel <you@example.com>
2024-07-29 10:03:46 +0000

    learned about hero animation allowing to animate a widget through  two pages

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:ftest/pages/gratitude.dart';
+import 'fly.dart';
 
-import 'about.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -9,60 +8,37 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  String _howAreYou = "...";
-
-  void _openPageAbout(
-      {required BuildContext context, bool fullscreenDialog = false}) {
-    Navigator.push(
-        context,
-        MaterialPageRoute(
-          fullscreenDialog: fullscreenDialog,
-          builder: (context) => About(),
-        ));
-  }
-
-  void _openPageGratitude(
-      {required BuildContext context, bool fullscreenDialog = false}) async {
-    final String _gratitudeResponse = await Navigator.push(
-        context,
-        MaterialPageRoute(
-          fullscreenDialog: fullscreenDialog,
-          builder: (context) => Gratitude(radioGroupValue: -1),
-        ));
-    _howAreYou = _gratitudeResponse ?? '';
-    setState(() {
-      
-    });
-  }
+  
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Navigator"),
-        actions: [
-          IconButton(
-              onPressed: () => _openPageAbout(
-                    context: context,
-                    fullscreenDialog: true,
-                  ),
-              icon: Icon(Icons.info))
-        ],
+        title: const Text("Hero Navigation Animation"),    
       ),
       body: SafeArea(
-        child: Padding(
+        child:Padding(
           padding: EdgeInsets.all(16.0),
-          child: Text(
-            "Grateful for:$_howAreYou ",
-            style: TextStyle(fontSize: 30.0),
+          child: GestureDetector(
+            child: const Hero(
+              tag: "format_paint", 
+              child: Icon(
+                Icons.format_paint,
+                color: Colors.lightBlue,
+                size: 120.0,
+              )
+              ),
+              onTap: () {
+                Navigator.push(
+                  context, MaterialPageRoute(
+                    builder: (context) => Fly() 
+                    )
+                );
+              },
           ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => _openPageGratitude(context: context),
-        tooltip: "About",
-        child: Icon(Icons.sentiment_satisfied),
-      ),
+        )
+
     );
   }
 }
